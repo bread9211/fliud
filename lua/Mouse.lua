ab33b2d68f3062d050844fad2f93ac02ba84ee95
bread9211 <sunnyfang921@gmail.com>
2022-10-03 15:43:12 -0700

    Mouse.lua

--- lua/Mouse.lua
+++ lua/Mouse.lua
@@ -28,4 +28,39 @@ function Mouse:init()
     document.body:addEventListener("touchmove", self:onDocumentTouchMove(), false)
 end
 
+function Mouse:setCoords(x, y)
+    if (self.timer) then
+        window.clearTimeout(self.timer)
+    end
+    self.coords.set((x / Common.width) * 2 - 1, -(y / Common.height) * 2 + 1)
+    self.mouseMoved = true
+    self.timer = window.setTimeout(function ()
+        self.mouseMoved = false
+    end, 100)
+end
+
+function Mouse:onDocumentMouseMove(event)
+    self:setCoords(event.clientX, event.clientY)
+end
+
+function Mouse:onDocumentTouchStart(event)
+    if (event.touches.length == 1) then
+        self:setCoords(event.touches[0].pageX, event.touches[0].pageY)
+    end
+end
+
+function Mouse:onDocumentTouchMove(event)
+    if (event.touches.length == 1) then
+        self.setCoords(event.touches[0].pageX, event.touches[0].pageY)
+    end
+end
+
+function Mouse:update()
+    self.diff:subVectors(self.coords, self.coords_old)
+    self.coords_old:copy(self.coords)
+    if (self.coords_old.x == 0 and self.coords_old.y == 0) then
+        self.diff:set(0, 0)
+    end
+end
+
 return Mouse:new()
